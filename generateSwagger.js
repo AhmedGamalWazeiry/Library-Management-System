159891898f1c171298108324d63f270f184df9db
unknown <ahmedgamal1452@gmail.com>
2024-01-23 19:39:17 +0200

    add swagger

--- generateSwagger.js
+++ generateSwagger.js
@@ -1,6 +1,6 @@
 const swaggerAutogen = require("swagger-autogen")();
 
 const outputFile = "./swagger_output3.json";
-const endpointsFiles = ["./src/authors/routes.js"]; // Add any additional files with your routes here
+const endpointsFiles = ["./src/borrowedBooks/routes.js"]; // Add any additional files with your routes here
 
 swaggerAutogen(outputFile, endpointsFiles);
